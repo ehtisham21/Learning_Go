717ea93cd0e3c0ffa3932692ba03b80e2cca2be7
ehtisham21 <buttehtisham123@gmail.com>
2026-06-16 15:54:11 +0500

    Starting Go Language

---
+++ 01_setup/main.go
@@ -0,0 +1,13 @@
+package main // Package main is the entry point for an executable Go program
+
+import "fmt" // Import the fmt package for printing output
+
+// main is the starting function of the program
+func main() {
+
+	// Print a success message to the console
+	fmt.Println("Go setup completed successfully!")
+
+	// Print a welcome message
+	fmt.Println("Welcome to my Go learning journey.")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ehtisham21/go-learning
+
+go 1.25.0
