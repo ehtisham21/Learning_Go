dc6de1010f5d27efcf31862b8ebba89873af1f23
ehtisham21 <buttehtisham123@gmail.com>
2026-06-16 17:33:37 +0500

    Learning Go

---
+++ 03_comments/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// This is a single-line comment
+
+func main() {
+	// Print a message to the console
+	fmt.Println("Hello, Go!")
+
+	/*
+	   This is a multi-line comment.
+	   Go ignores everything inside it.
+	*/
+
+	fmt.Println("Learning comments in Go")
+}
